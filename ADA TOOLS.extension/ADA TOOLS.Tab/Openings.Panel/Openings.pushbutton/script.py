683b9c0e92a2a05fdd1170e62e8d32113fbb8cce
Alaronza55 <dalmog.dav@gmail.com>
2024-10-20 16:59:58 +0200

    Added description and roadmap to Openings script

--- ADA TOOLS.extension/ADA TOOLS.Tab/Openings.Panel/Openings.pushbutton/script.py
+++ ADA TOOLS.extension/ADA TOOLS.Tab/Openings.Panel/Openings.pushbutton/script.py
@@ -1,3 +1,31 @@
+# -*- coding: utf-8 -*-
+__title__ = "Create Openings Tool"   # Name of the button displayed in Revit
+__author__ = "Almog Davodson"
+__doc__ = """Version = 1.0
+_____________________________________________________________________
+Description:
+
+Create 3D Generic Model at the intersection between a Surface (Horizontal or Vertical)
+and Pipe. 
+
+Future Features / Roadmap :
+- Make same process between two elements in link
+- Integrate Conducts - HVAC Ducts
+- Set Offset to Generic Model if needed.
+- Make auto caracerization of opening
+_____________________________________________________________________
+How-to:
+
+1. Select Element of surface
+2. Select Pipe
+
+_____________________________________________________________________
+Last update:
+- [20.10.2024] - 1.0 First coding attempt
+_____________________________________________________________________
+Author:     Almog Davidson"""
+
+
 #Imports
 import sys
 from Autodesk.Revit.DB import (Transaction)
